b2493f3bb9252b3ce2ca6edf841b4d1445b46abb
peony_cyber <peony_0702@qq.com>
2026-03-21 14:29:41 +0800

    upgrade with comment and functions

--- src/behavior_tree/src/GameLoop.cpp
+++ src/behavior_tree/src/GameLoop.cpp
@@ -141,7 +141,7 @@ namespace BehaviorTree {
                 gimbalControlData.FireCode.Rotate = 1;
             }
         } else {
-            gimbalControlData.FireCode.Rotate = 1;
+            gimbalControlData.FireCode.Rotate = 0;
         }
         if (config.AimDebugSettings.StopRotate) gimbalControlData.FireCode.Rotate = 0;
 
@@ -208,7 +208,7 @@ namespace BehaviorTree {
                     }
                     const auto current_time = std::chrono::steady_clock::now();
                     nextAngles = GimbalAnglesType{
-                        static_cast<AngleType>(gimbalAngles.Yaw + 8 * delta_yaw),
+                        static_cast<AngleType>(gimbalAngles.Yaw + 3 * delta_yaw),
                         AngleType{-0.0f + pitch_wave.Produce(current_time) * 3.0f}
                     };
 
